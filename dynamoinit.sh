0680760816cbaa626251f30df1f1050aad614fb3
dvdg6566 <czhdaniel@gmail.com>
2023-01-27 16:31:47 +0800

    Added dynamo command to create user

--- dynamoinit.sh
+++ dynamoinit.sh
@@ -5,3 +5,6 @@ aws dynamodb put-item --table-name codebreakercontest-global-counters \
 --item '{"counterId": {"S": "clarificationId"}, "value": {"N": "0"}}'
 
 # TODO: Add thing to initialise first user
+
+aws dynamodb put-item --table-name codebreakercontest-users \
+--item '{"username": {"S": "0rang3"}, "role": {"S": "admin"}, "problemScores": {"M" : {}}, "fullname": {"S": "Default admin"}, "contest": {"S": ""}}'
